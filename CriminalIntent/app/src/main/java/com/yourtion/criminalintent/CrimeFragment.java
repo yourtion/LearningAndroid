a128e163ce79ffc78c1f44c4045ddbc214b775f9
YourtionGuo <yourtion@gmail.com>
2016-05-05 14:03:03 +0800

    #7.5 创建UI fragment

---
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeFragment.java
@@ -0,0 +1,52 @@
+package com.yourtion.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by Yourtion on 5/5/16.
+ */
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This space intentionally left blank
+            }
+        });
+
+        return v;
+    }
+}
