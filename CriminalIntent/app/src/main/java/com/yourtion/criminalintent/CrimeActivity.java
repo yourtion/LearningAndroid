741c82f116f2bfc53c1b282b97df0a0dee945a1f
YourtionGuo <yourtion@gmail.com>
2016-05-05 14:37:23 +0800

    #7.6 添加UI fragment到FragmentManager

--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeActivity.java
@@ -1,13 +1,25 @@
 package com.yourtion.criminalintent;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 }
--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeFragment.java
@@ -1,8 +1,8 @@
 package com.yourtion.criminalintent;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
