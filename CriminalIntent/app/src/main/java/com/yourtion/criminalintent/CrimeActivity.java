bd9ee6c6cda48ec9cc3caeb7d4ddad3c57c0e0bc
YourtionGuo <yourtion@gmail.com>
2016-05-11 11:20:31 +0800

    #10.2 fragment argument

--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeActivity.java
@@ -2,9 +2,12 @@ package com.yourtion.criminalintent;
 
 import android.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
