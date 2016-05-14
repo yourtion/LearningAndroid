7252fcd90d6d91038258b5cd8fd827c30c10ede1
YourtionGuo <yourtion@gmail.com>
2016-05-14 18:21:41 +0800

    #13.3 创建HelloMoonFragment

---
+++ HelloMoon/app/src/main/java/com/yourtion/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,26 @@
+package com.yourtion.hellomoon;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Yourtion on 5/14/16.
+ */
+public class HelloMoonFragment extends Fragment {
+    private Button mPlayButton;
+    private Button mStopButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+        mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
+        mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
+        return v;
+    }
+}
