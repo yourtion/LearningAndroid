8522259010e6be4b921f644ecef737183275cfca
YourtionGuo <yourtion@gmail.com>
2016-05-14 13:37:33 +0800

    Init Project HelloMoon

---
+++ HelloMoon/app/src/main/java/com/yourtion/hellomoon/HelloMoonActivity.java
@@ -0,0 +1,13 @@
+package com.yourtion.hellomoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloMoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+}
