6dc82d9f583611870ddda2b57af825bceeff3371
YourtionGuo <yourtion@gmail.com>
2016-05-16 18:07:04 +0800

    #16.1 选项菜单

--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeLab.java
@@ -16,13 +16,10 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
+    }
 
-        for (int i = 0; i < 100; i++) {
-            Crime c = new Crime();
-            c.setTitle("Crime #" + i);
-            c.setSolved(i % 2 == 0);
-            mCrimes.add(c);
-        }
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public static CrimeLab get(Context c) {
