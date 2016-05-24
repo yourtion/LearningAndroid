6beacc7f4178cfd88e31ae00fb9ea52c9d88398b
YourtionGuo <yourtion@gmail.com>
2016-05-24 11:51:05 +0800

    #21.4.2 发送陋习报告

--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/CrimeFragment.java
@@ -14,6 +14,7 @@ import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -163,6 +164,19 @@ public class CrimeFragment extends Fragment {
         if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) && !pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)) {
             mPhotoButton.setEnabled(false);
         }
+
+        Button reportButton = (Button) v.findViewById(R.id.crime_reportButton);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+                i = Intent.createChooser(i, getString(R.string.send_report));
+                startActivity(i);
+            }
+        });
 
         return v;
     }
