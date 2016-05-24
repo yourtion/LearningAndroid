c5641eac09a51a836a013549af0ee2de481d07f9
YourtionGuo <yourtion@gmail.com>
2016-05-24 11:29:45 +0800

    #21.2 添加嫌疑人信息至模型层

--- CriminalIntent/app/src/main/java/com/yourtion/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/yourtion/criminalintent/Crime.java
@@ -16,12 +16,14 @@ public class Crime {
     private static final String JSON_SOLVED = "solved";
     private static final String JSON_DATE = "date";
     private static final String JSON_PHOTO = "photo";
+    private static final String JSON_SUSPECT = "suspect";
 
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
     private Photo mPhoto;
+    private String mSuspect;
 
     public Crime() {
         mId = UUID.randomUUID();
@@ -36,6 +38,9 @@ public class Crime {
         if (json.has(JSON_PHOTO)) {
             mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
         }
+        if (json.has(JSON_SUSPECT)) {
+            mSuspect = json.getString(JSON_SUSPECT);
+        }
     }
 
     public JSONObject toJSON() throws JSONException {
@@ -47,6 +52,7 @@ public class Crime {
         if (mPhoto != null) {
             json.put(JSON_PHOTO, mPhoto.toJSON());
         }
+        json.put(JSON_SUSPECT, mSuspect);
         return json;
     }
 
@@ -89,5 +95,13 @@ public class Crime {
 
     public void setPhoto(Photo photo) {
         mPhoto = photo;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 }
