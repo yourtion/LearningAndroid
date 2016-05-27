0c47ab3abb02ba3f5b3fd8a70147509a27e5c525
YourtionGuo <yourtion@gmail.com>
2016-05-27 12:13:59 +0800

    Init NerdLauncher Project

---
+++ NerdLauncher/app/src/main/java/com/yourtion/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.yourtion.nerdlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nerd_launcher);
+    }
+}
