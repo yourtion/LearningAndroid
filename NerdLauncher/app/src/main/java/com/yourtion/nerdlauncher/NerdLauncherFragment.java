6ea36afa581e7bd1ca5566da9f4dd6f9c686de1c
YourtionGuo <yourtion@gmail.com>
2016-05-27 15:08:23 +0800

    #23.4 任务与后退栈

--- NerdLauncher/app/src/main/java/com/yourtion/nerdlauncher/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/yourtion/nerdlauncher/NerdLauncherFragment.java
@@ -70,6 +70,7 @@ public class NerdLauncherFragment extends ListFragment {
 
         Intent i = new Intent(Intent.ACTION_MAIN);
         i.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
     }
 }
