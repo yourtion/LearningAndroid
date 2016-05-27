c9c88f593d26db2f5b690a3fa0007bc23d87b96d
YourtionGuo <yourtion@gmail.com>
2016-05-27 12:43:08 +0800

    #23.2 解析隐式intent

--- NerdLauncher/app/src/main/java/com/yourtion/nerdlauncher/NerdLauncherFragment.java
+++ NerdLauncher/app/src/main/java/com/yourtion/nerdlauncher/NerdLauncherFragment.java
@@ -1,9 +1,63 @@
 package com.yourtion.nerdlauncher;
 
 import android.app.ListFragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by Yourtion on 5/27/16.
  */
 public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        final PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo lhs, ResolveInfo rhs) {
+                return String.CASE_INSENSITIVE_ORDER
+                        .compare(lhs.loadLabel(pm).toString(), rhs.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                activities)
+        {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View v = super.getView(position, convertView, parent);
+                // Documentation says that simple_list_item_1 is a TextView so cast it so that you can set its text value
+                TextView tv = (TextView)v;
+                ResolveInfo ri = getItem(position);
+                tv.setText(ri.loadLabel(pm));
+                return v;
+
+            }
+        };
+        setListAdapter(adapter);
+
+    }
 }
