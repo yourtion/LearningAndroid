4661c4352b888ab3f256578436cf88712558983d
YourtionGuo <yourtion@gmail.com>
2016-05-29 12:09:38 +0800

    Init RemoteControl Project

---
+++ RemoteControl/app/src/main/java/com/yourtion/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,13 @@
+package com.yourtion.remotecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_control);
+    }
+}
