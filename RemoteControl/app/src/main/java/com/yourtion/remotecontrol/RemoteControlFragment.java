fba5bbf2d719bb9888fe21459baf7bf9f03fb3d8
YourtionGuo <yourtion@gmail.com>
2016-05-29 14:06:45 +0800

    #24.3 完善布局定义

--- RemoteControl/app/src/main/java/com/yourtion/remotecontrol/RemoteControlFragment.java
+++ RemoteControl/app/src/main/java/com/yourtion/remotecontrol/RemoteControlFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 /**
@@ -40,12 +42,34 @@ public class RemoteControlFragment extends Fragment {
             }
         };
 
-        Button zeroButton = (Button) v.findViewById(R.id.fragment_remote_control_zeroButton);
-        zeroButton.setOnClickListener(numberButtonListener);
-        Button oneButton = (Button) v.findViewById(R.id.fragment_remote_control_oneButton);
-        oneButton.setOnClickListener(numberButtonListener);
+        TableLayout tableLayout = (TableLayout) v.findViewById(R.id.fragment_remote_control_tableLayout);
+        int number = 1;
+        for (int i = 2; i < tableLayout.getChildCount() - 1; i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for (int j = 0; j < row.getChildCount(); j++) {
+                Button button = (Button) row.getChildAt(j);
+                button.setText("" + number);
+                button.setOnClickListener(numberButtonListener);
+                number++;
+            }
+        }
 
-        Button enterButton = (Button) v.findViewById(R.id.fragment_remote_control_enterButton);
+        TableRow bottomRow = (TableRow) tableLayout.getChildAt(tableLayout.getChildCount() - 1);
+        Button deleteButton = (Button) bottomRow.getChildAt(0);
+        deleteButton.setText("Delete");
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWorkingTextView.setText("0");
+            }
+        });
+
+        Button zeroButton = (Button) bottomRow.getChildAt(1);
+        zeroButton.setText("0");
+        zeroButton.setOnClickListener(numberButtonListener);
+
+        Button enterButton = (Button) bottomRow.getChildAt(2);
+        enterButton.setText("Enter");
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
