6b4d4dfd44f9a9256dfa829fd249929f274ad099
YourtionGuo <yourtion@gmail.com>
2016-05-29 15:51:13 +0800

    Init PhotoGallery Project

---
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package com.yourtion.photogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+}
