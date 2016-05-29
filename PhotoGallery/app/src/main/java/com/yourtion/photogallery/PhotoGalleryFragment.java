98028dd3c3129da4d7f32e1dedecba4832acce42
YourtionGuo <yourtion@gmail.com>
2016-05-29 16:17:31 +0800

    #26.3 使用AsyncTask在后台线程上运行代码

--- PhotoGallery/app/src/main/java/com/yourtion/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/PhotoGalleryFragment.java
@@ -1,23 +1,30 @@
 package com.yourtion.photogallery;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by Yourtion on 5/29/16.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
     private GridView mGridView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -28,5 +35,18 @@ public class PhotoGalleryFragment extends Fragment {
         mGridView = (GridView) v.findViewById(R.id.gridView);
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.baidu.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }
