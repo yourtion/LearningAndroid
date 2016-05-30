24086ef30d8b652240a908da6a4acfc1c8e82c4f
YourtionGuo <yourtion@gmail.com>
2016-05-30 17:12:13 +0800

    #29.2 服务的作用

--- PhotoGallery/app/src/main/java/com/yourtion/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/PollService.java
@@ -1,7 +1,9 @@
 package com.yourtion.photogallery;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -16,6 +18,10 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        @SuppressWarnings("deprecation")
+        boolean isNetworkAvailable = cm.getBackgroundDataSetting() && cm.getActiveNetworkInfo() != null;
+        if (!isNetworkAvailable) return;
         Log.i(TAG, "Received an intent: " + intent);
     }
 }
