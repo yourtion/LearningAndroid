75c44819228fbd6ea4200244a84108da30d66a36
YourtionGuo <yourtion@gmail.com>
2016-05-30 17:54:09 +0800

    #29.6 通知信息

--- PhotoGallery/app/src/main/java/com/yourtion/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/PollService.java
@@ -2,12 +2,16 @@ package com.yourtion.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -17,7 +21,7 @@ import java.util.ArrayList;
  */
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
-    private static final int POLL_INTERVAL = 1000 * 15;// 15 second
+    private static final int POLL_INTERVAL = 1000 * 60 * 15;// 15 minute
 
     public PollService() {
         super(TAG);
@@ -66,6 +70,20 @@ public class PollService extends IntentService {
         String resultId = items.get(0).getId();
         if (!resultId.equals(lastResultId)) {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources r = getResources();
+            PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, PhotoGalleryActivity.class), 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(r.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentText(r.getString(R.string.new_pictures_text))
+                    .setContentTitle(r.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(0, notification);
         } else {
             Log.i(TAG, "Got an old result: " + resultId);
         }
