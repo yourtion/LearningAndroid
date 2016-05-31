27e8992c4a3befcd3195fc4f2a0029f307bc18f0
YourtionGuo <yourtion@gmail.com>
2016-05-31 11:37:10 +0800

    #31.1 最后一段Flickr数据

--- PhotoGallery/app/src/main/java/com/yourtion/photogallery/GalleryItem.java
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/GalleryItem.java
@@ -7,6 +7,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String toString() {
         return mCaption;
@@ -34,5 +35,17 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public String getPhotoPageUrl() {
+        return "http://www.flickr.com/photos/" + mOwner + "/" + mId;
     }
 }
