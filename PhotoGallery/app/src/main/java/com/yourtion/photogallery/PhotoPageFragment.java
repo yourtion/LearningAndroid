2ec859138ca322e456519068031d7a5fbe8ad25f
YourtionGuo <yourtion@gmail.com>
2016-05-31 12:40:18 +0800

    #31.3.1 使用WebChromeClient优化WebView的显示

--- PhotoGallery/app/src/main/java/com/yourtion/photogallery/PhotoPageFragment.java
+++ PhotoGallery/app/src/main/java/com/yourtion/photogallery/PhotoPageFragment.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Yourtion on 5/31/16.
@@ -28,13 +31,35 @@ public class PhotoPageFragment extends VisibleFragment {
     @SuppressLint("SetJavaScriptEnabled")
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        final ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        final TextView titleTextView = (TextView) v.findViewById(R.id.titleTextView);
         mWebView = (WebView) v.findViewById(R.id.webView);
+
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 return false;
             }
         });
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                titleTextView.setText(title);
+            }
+        });
+
         mWebView.loadUrl(mUrl);
         return v;
     }
