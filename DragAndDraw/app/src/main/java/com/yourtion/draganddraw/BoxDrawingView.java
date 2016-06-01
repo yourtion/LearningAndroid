7b681fca45f4a3da7c20c65cfb6f3239787a8582
YourtionGuo <yourtion@gmail.com>
2016-06-01 11:18:06 +0800

    #32.4 onDraw(...)方法内的图形绘制

--- DragAndDraw/app/src/main/java/com/yourtion/draganddraw/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/yourtion/draganddraw/BoxDrawingView.java
@@ -1,6 +1,8 @@
 package com.yourtion.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -17,6 +19,8 @@ public class BoxDrawingView extends View {
 
     private Box mCurrentBox;
     private ArrayList<Box> mBoxes = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -26,6 +30,29 @@ public class BoxDrawingView extends View {
     // Used when inflating the view from XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        // Paint the boxes a nice semitransparent red (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        // Paint the background off-white
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Fill the background
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxes) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 
     @Override
@@ -38,12 +65,12 @@ public class BoxDrawingView extends View {
                 Log.i(TAG, "ACTION_DOWN");
                 // Reset drawing state
                 mCurrentBox = new Box(curr);
+                mBoxes.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 Log.i(TAG, "ACTION_MOVE");
                 if (mCurrentBox != null) {
                     mCurrentBox.setCurrent(curr);
-                    mBoxes.add(mCurrentBox);
                     invalidate();
                 }
                 break;
