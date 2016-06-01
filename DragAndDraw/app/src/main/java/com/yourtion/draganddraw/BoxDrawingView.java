b55549c3e220aa93ff10c24fcd4bdf18dcc4ae4e
YourtionGuo <yourtion@gmail.com>
2016-06-01 10:35:32 +0800

    #32.2 创建定制视图

---
+++ DragAndDraw/app/src/main/java/com/yourtion/draganddraw/BoxDrawingView.java
@@ -0,0 +1,20 @@
+package com.yourtion.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Yourtion on 6/1/16.
+ */
+public class BoxDrawingView extends View {
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
