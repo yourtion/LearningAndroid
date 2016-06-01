2b68f85638cdcd3758da9758da4aa5aaa2794f85
YourtionGuo <yourtion@gmail.com>
2016-06-01 15:17:24 +0800

    #33.5 快速定位：最近一次地理位置

--- RunTracker/app/src/main/java/com/yourtion/runtracker/RunManager.java
+++ RunTracker/app/src/main/java/com/yourtion/runtracker/RunManager.java
@@ -3,6 +3,7 @@ package com.yourtion.runtracker;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.location.LocationManager;
 
 /**
@@ -39,9 +40,23 @@ public class RunManager {
     public void startLocationUpdates() {
         String provider = LocationManager.GPS_PROVIDER;
 
+        // Get the last known location and broadcast it if you have one
+        Location lastKnown = mLocationManager.getLastKnownLocation(provider);
+        if (lastKnown != null) {
+            // Reset the time to now
+            lastKnown.setTime(System.currentTimeMillis());
+            broadcastLocation(lastKnown);
+        }
+
         // Start updates from the location manager
         PendingIntent pi = getLocationPendingIntent(true);
         mLocationManager.requestLocationUpdates(provider, 0, 0, pi);
+    }
+
+    private void broadcastLocation(Location location) {
+        Intent broadcast = new Intent(ACTION_LOCATION);
+        broadcast.putExtra(LocationManager.KEY_LOCATION_CHANGED, location);
+        mAppContext.sendBroadcast(broadcast);
     }
 
     public void stopLocationUpdates() {
