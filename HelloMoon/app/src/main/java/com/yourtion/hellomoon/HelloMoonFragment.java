c344c2d1ba8de403421fbe9261a77783a73925c8
YourtionGuo <yourtion@gmail.com>
2016-06-03 11:56:39 +0800

    update HelloMoon

--- HelloMoon/app/src/main/java/com/yourtion/hellomoon/HelloMoonFragment.java
+++ HelloMoon/app/src/main/java/com/yourtion/hellomoon/HelloMoonFragment.java
@@ -2,7 +2,6 @@ package com.yourtion.hellomoon;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +22,6 @@ public class HelloMoonFragment extends Fragment {
         setRetainInstance(true);
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
