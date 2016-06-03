8a25b047278bf475bf37466db01a055f1a4a1c74
YourtionGuo <yourtion@gmail.com>
2016-06-03 12:00:38 +0800

    update RemoteControl

--- RemoteControl/app/src/main/java/com/yourtion/remotecontrol/RemoteControlFragment.java
+++ RemoteControl/app/src/main/java/com/yourtion/remotecontrol/RemoteControlFragment.java
@@ -2,7 +2,6 @@ package com.yourtion.remotecontrol;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ public class RemoteControlFragment extends Fragment {
     private TextView mSelectedTextView;
     private TextView mWorkingTextView;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_remote_control, container, false);
