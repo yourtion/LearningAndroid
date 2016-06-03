829c2564ff12250da0bb04df328c820a79aadb8d
YourtionGuo <yourtion@gmail.com>
2016-06-03 12:06:16 +0800

    update DragAndDraw

--- DragAndDraw/app/src/main/java/com/yourtion/draganddraw/DragAndDrawFragment.java
+++ DragAndDraw/app/src/main/java/com/yourtion/draganddraw/DragAndDrawFragment.java
@@ -2,7 +2,6 @@ package com.yourtion.draganddraw;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +26,6 @@ public class DragAndDrawFragment extends Fragment {
         }
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
